c45b26aedb719b1b6cee707799e3cd127c9b5cd8
Muhammad Andi Ubaidillah <muhandiubaid9601@gmail.com>
2025-09-17 16:13:10 +0800

    Update Preprocessing.py

--- Image Classification 1-9/src/Preprocessing.py
+++ Image Classification 1-9/src/Preprocessing.py
@@ -11,6 +11,15 @@ X_train = X_train.reshape(-1,28,28,1)
 X_test = X_test.reshape(-1,28,28,1)
 y_train = to_categorical(y_train, num_classes = 10)
 
+# Visualisasi beberapa contoh gambar
+plt.figure(figsize=(15,4.5))
+for i in range(30):
+  plt.subplot(3, 10, i+1)
+  plt.imshow(X_train[i].reshape((28,28)), cmap=plt.cm.binary)
+  plt.axis('off')
+plt.subplots_adjust(wspace=0.1, hspace=-0.1)
+plt.show()
+
 from sklearn.model_selection import train_test_split
 
 X_train_sp, X_val, y_train_sp, y_val = train_test_split(
