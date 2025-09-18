fe19b9d8c80848239c52338313413e9988770481
Muhammad Andi Ubaidillah <muhandiubaid9601@gmail.com>
2025-09-18 10:54:04 +0800

    Create Preprocessing.py

---
+++ Image Classification 0-9 No NAS/src/Preprocessing.py
@@ -0,0 +1,31 @@
+X_train = train.drop('label', axis=1).to_numpy()
+y_train = train['label'].to_numpy()
+print(f'Shape train X: {X_train.shape}, y:{y_train.shape}')
+
+#Preprocessing
+from tensorflow.keras.utils import to_categorical
+
+X_test = test.to_numpy()
+X_train = X_train / 255.0
+X_test = X_test / 255.0
+X_train = X_train.reshape(-1,28,28,1)
+X_test = X_test.reshape(-1,28,28,1)
+y_train = to_categorical(y_train, num_classes = 10)
+
+from sklearn.model_selection import train_test_split
+
+X_train_sp, X_val, y_train_sp, y_val = train_test_split(
+    X_train, y_train, test_size=0.1, random_state=42
+)
+print('Train:',X_train_sp.shape, y_train_sp.shape)
+print('Val :',X_val.shape, y_val.shape)
+
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+# CREATE MORE IMAGES VIA DATA AUGMENTATION
+datagen = ImageDataGenerator(
+        rotation_range=10,
+        zoom_range = 0.10,
+        width_shift_range=0.1,
+        height_shift_range=0.1)
+datagen.fit(X_train_sp)
